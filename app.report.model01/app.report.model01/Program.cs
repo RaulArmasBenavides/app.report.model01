cf8c3ae855fd616480ca96b838eb897ef9edee2b
Raúl Armas <raularmasbx@gmail.com>
2022-03-06 18:26:50 -0500

    new

---
+++ app.report.model01/app.report.model01/Program.cs
@@ -0,0 +1,20 @@
+﻿using app.report.model01.View;
+using System;
+using System.Windows.Forms;
+
+namespace app.report.model01
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmReporte());
+        }
+    }
+}
---
+++ app.report.model01/app.report.model01/View/frmReporte.cs
@@ -0,0 +1,25 @@
+﻿using app.report.model01.Reportes;
+using System;
+using System.Windows.Forms;
+
+namespace app.report.model01.View
+{
+    public partial class frmReporte : Form
+    {
+        public frmReporte()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReporte_Load(object sender, EventArgs e)
+        {
+            ListaPrecios f = new ListaPrecios();
+            crvInforme.ReportSource = f;
+        }
+
+        private void crvInforme_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
